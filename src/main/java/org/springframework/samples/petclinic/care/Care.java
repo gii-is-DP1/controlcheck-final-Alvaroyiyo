a5fdb3ac7fd2e51bd3daecc966e8555ea7f17073
Alvaro <alvsevcab@alum.us.es>
2022-01-21 12:02:47 +0100

    Entrega Alvsevcab convocatoria DP

--- src/main/java/org/springframework/samples/petclinic/care/Care.java
+++ src/main/java/org/springframework/samples/petclinic/care/Care.java
@@ -2,15 +2,48 @@ package org.springframework.samples.petclinic.care;
 
 import java.util.Set;
 
+import javax.persistence.CascadeType;
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.FetchType;
+import javax.persistence.JoinColumn;
+import javax.persistence.JoinTable;
+import javax.persistence.ManyToMany;
+import javax.persistence.Table;
+import javax.validation.constraints.NotEmpty;
+import javax.validation.constraints.NotNull;
+import javax.validation.constraints.Size;
+
+import org.hibernate.annotations.Cascade;
+import org.hibernate.validator.constraints.Length;
+import org.springframework.samples.petclinic.model.BaseEntity;
 import org.springframework.samples.petclinic.pet.PetType;
+import org.springframework.samples.petclinic.vet.Specialty;
 
 import lombok.Getter;
 import lombok.Setter;
 
 @Getter
 @Setter
-public class Care {
+@Entity
+@Table
+public class Care extends BaseEntity{
+	@Length(min = 5, max = 30)
+	@NotEmpty
+	@Column(unique = true)
     String name;
+	@NotEmpty
     String description;
+	@ManyToMany(cascade = {
+	        CascadeType.ALL,
+	        CascadeType.MERGE
+	    })
+	
+	@NotEmpty
     Set<PetType> compatiblePetTypes;
+	@ManyToMany(cascade = {
+	        CascadeType.ALL,
+	        CascadeType.MERGE
+	    })
+	Set<Care> incompatibleCares;
 }
--- src/main/java/org/springframework/samples/petclinic/care/CareProvision.java
+++ src/main/java/org/springframework/samples/petclinic/care/CareProvision.java
@@ -1,14 +1,35 @@
 package org.springframework.samples.petclinic.care;
 
+import javax.persistence.CascadeType;
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.ManyToOne;
+import javax.persistence.Table;
+import javax.validation.constraints.Min;
+import javax.validation.constraints.NotNull;
+import javax.validation.constraints.PositiveOrZero;
+import javax.validation.constraints.Size;
+
+import org.springframework.samples.petclinic.model.BaseEntity;
 import org.springframework.samples.petclinic.pet.Visit;
+
 
 import lombok.Getter;
 import lombok.Setter;
 
 @Getter
 @Setter
-public class CareProvision {   
-    double duration;
-    Visit visit;
-    Care care;   
+@Entity
+@Table
+public class CareProvision extends BaseEntity{   
+	   
+	@Min(0)
+	double duration;
+	
+	@ManyToOne(optional = true)
+	Visit visit;
+	
+	@ManyToOne
+	@NotNull
+	Care care;
 }
